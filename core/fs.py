614c76afd8bbd455c1f33f33157c012d04198096
carlosvin <carlosvin@gmail.com>
2014-04-08 23:21:22 +0200

    starting

--- core/fs.py
+++ core/fs.py
@@ -38,9 +38,14 @@ class Directory(File):
         self._is_dir = True
         self._files = dict()
 
+    def get_guaranteed_dir(self, split_path):
+        if not split_path[0] in self._files.keys():
+            self._files[split_path[0]] = Directory(name= split_path[0], child_path = split_path[1:-1])
+
 
 class FS:
     def __init__(self):
+        self._size = 0
         self.root = Directory('/')
 
     def add_file(self, from_local_file_path, to_remote_dir_path):
@@ -48,11 +53,16 @@ class FS:
             local_stat_info = os.stat(from_local_file_path)
             if local_stat_info.st_size > 0:
                 directory = self.get_guaranteed_dir(to_remote_dir_path)
+                self._size += local_stat_info.st_size
             else:
                 raise IOError('Invalid local file size: ' + local_stat_info.st_size)
         else:
             raise FileNotFoundError(from_local_file_path)
 
     def get_guaranteed_dir(self,to_remote_dir_path):
-        print(os.path.split(to_remote_dir_path))
-        return Directory('')
+        tmp_path = os.path.normpath(to_remote_dir_path)
+        return self.root.get_guaranteed_dir(tmp_path.split(os.path.sep))
+
+    @property
+    def size(self):
+        return self._size
--- core/test/fs_test.py
+++ core/test/fs_test.py
@@ -2,19 +2,28 @@ __author__ = 'carlos'
 
 import unittest
 from core.fs import FS, Directory, File
-
+import os
 
 class FsTestCase(unittest.TestCase):
 
     def test_initial_fs(self):
         filesystem = FS()
-        print(filesystem.root)
         self.assertEqual(filesystem.root, Directory('/') )
 
     def test_fs_size(self):
         filesystem = FS()
-        filesystem.add_file(from_local_file_path=__file__, to_remote_dir_path='/foo/bar/a/b/c')
-        filesystem.size()
+        dir_path = os.path.dirname(__file__)
+        init_file = os.path.join(dir_path, '__init__.py')
+        this_file = __file__
+
+        init_file_size = os.stat(init_file).st_size
+        this_file_size = os.stat(this_file).st_size
+
+        filesystem.add_file(from_local_file_path=init_file, to_remote_dir_path=dir_path)
+        self.assertEquals(init_file_size, filesystem.size)
+
+        filesystem.add_file(from_local_file_path=this_file, to_remote_dir_path=dir_path)
+        self.assertEquals(init_file_size + this_file_size, filesystem.size)
 
 
 if __name__ == '__main__':
